00ba411821081cfb412ec1ad1ef27482659adbd8
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-08-12 13:45:25 +0300

    feat: get data from api; refact: change function of input

--- src/App.js
+++ src/App.js
@@ -5,14 +5,53 @@ import Dashboard from './Dashboard';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <CityInput />
-      <DashboardPlaceholder />
-      <Dashboard />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weatherData: [],
+      value: ''
+    }
+  }
+
+  setValue(e) {
+    e.target.value = e.target.value.replace(/[^a-zA-Z0-9-]/gi, '');
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleAddWeather() {
+    const cityData = {
+      name: '',
+      temp: '',
+      icon: ''
+    };
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=58ca83824e1c0f434d211a07e20e3ad0`)
+      .then(response => {
+        if (response.status !== 200) throw new Error('status network not 200');
+        return response.json();
+      })
+      .then(data => {
+          cityData.name = data.name;
+          cityData.temp = Math.round(+data.main.temp - 273.15);
+          cityData.icon = data.weather[0].icon;
+      })
+      .catch(error => {
+        console.error(error);
+        cityData.name = 'Error';
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CityInput onChange={e => this.setValue(e)} />
+        <DashboardPlaceholder />
+        <Dashboard />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/CityInput.js
+++ src/CityInput.js
@@ -1,29 +1,16 @@
 import React from 'react';
 
-class CityInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ''
-    }
-  }
-  render() {
-    return (
-      <div className="city-input">
-        <input 
-          onChange={e => {
-            e.target.value = e.target.value.replace(/[^a-zA-Z0-9-]/gi, '');
-            this.setState({
-              value: e.target.value
-            })
-          }}
-          value={this.state.value}
-        />
-        <button>Add</button>
-        <button onClick={() => this.setState({ value: '' })}>Clear</button>
-      </div>
-    );
-  }
+const CityInput = props => {
+  return (
+    <div className="city-input">
+      <input 
+        onChange={props.onChange}
+        value={props.value}
+      />
+      <button onClick={props.handleAddWeather}>Add</button>
+      <button onClick={() => this.setState({ value: '' })}>Clear</button>
+    </div>
+  );
 };
 
 export default CityInput;
